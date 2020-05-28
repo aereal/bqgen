a95c0d29de3ba5a811bcb8919752dfc3015cf7aa
aereal <aereal@aereal.org>
2020-05-28 21:22:27 +0900

    Sketch Generate()

--- cli/cli.go
+++ cli/cli.go
@@ -2,8 +2,15 @@ package cli
 
 import (
 	"flag"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/aereal/bqgen/sql"
 )
 
 func NewApp(out, errOut io.Writer) *App {
@@ -24,6 +31,10 @@ type App struct {
 func (c *App) Run(argv []string) error {
 	fs := flag.NewFlagSet(argv[0], flag.ContinueOnError)
 	fs.SetOutput(c.errOut)
+	var (
+		tableOptions string
+	)
+	fs.StringVar(&tableOptions, "table-options", "", "additional table options")
 	err := fs.Parse(argv[1:])
 	if err == flag.ErrHelp {
 		return nil
@@ -31,5 +42,32 @@ func (c *App) Run(argv []string) error {
 	if err != nil {
 		return err
 	}
+	return c.cmdMain(tableOptions, fs.Args()...)
+}
+
+func (c *App) cmdMain(tableOptions string, args ...string) error {
+	b := sql.NewBuilder(&sql.Options{TableOptions: tableOptions})
+	for _, schemaFile := range args {
+		raw, err := ioutil.ReadFile(schemaFile)
+		if err != nil {
+			return err
+		}
+		schema, err := bigquery.SchemaFromJSON(raw)
+		if err != nil {
+			return err
+		}
+		b.Consume(schema, buildTableName(schemaFile))
+	}
+	ddl, err := b.Generate()
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(c.out, ddl)
 	return nil
+}
+
+func buildTableName(fn string) string {
+	base := filepath.Base(fn)
+	ext := filepath.Ext(fn)
+	return strings.Replace(base, ext, "", 1)
 }
